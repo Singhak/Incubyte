16a4f9beb37fd3cabfc7152837020a779821eaed
singhak <anilsingh.singh09@gmail.com>
2025-04-16 16:39:54 +0530

    feat: add isStringHasSpecialChar function and update add function to handle numeric strings

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 export const add = (numbersString: string) => {
     // if string is empty return 0
     if (!numbersString?.trim().length) return 0;
+    // if string without any delimiter return string as number
+    if (!isStringHasSpecialChar(numbersString)) return parseInt(numbersString);
+}
+
+function isStringHasSpecialChar(str: string) {
+    // we will check whether string has only number or any other char
+    const specialRegex = /[^\d]/;
+    // return true if other string consist other than number
+    return specialRegex.test(str)
 }
--- test.ts
+++ test.ts
@@ -4,4 +4,8 @@ describe('Add', () => {
     test('should return 0 if string empty', () => {
         expect(add("")).toBe(0)
     })
+
+    test('should return string as number if string consist only number', () => {
+        expect(add("123")).toBe(123)
+    })
 })
